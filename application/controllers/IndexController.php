2943a685422918f6d6fa2057a0f5b12c9a1fd5cf
Thomas Laumann <laumann.thomas@gmail.com>
2011-07-24 21:53:17 +0200

    playing with session management

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,19 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+	$s = new Zend_Session_Namespace();
+
+	var_dump($s);
+
+	if (!isset($s->counter))
+		$s->counter = 1000;
+	else
+		$s->counter++;
+	
+	$this->view->counter = $s->counter;
+
+	if ($s->counter > 1999)
+		unset($s->counter);
     }
 
     public function indexAction()
@@ -13,6 +26,4 @@ class IndexController extends Zend_Controller_Action
         // action body
     }
 
-
 }
-
