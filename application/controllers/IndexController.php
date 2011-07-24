110ef2b7505cacfe97dd43b7fb71988868697c24
Thomas Laumann <laumann.thomas@gmail.com>
2011-07-25 01:40:42 +0200

    removed session counter test code and loggedInUser function definition from IndexController

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,39 +6,10 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
-	$s = new Zend_Session_Namespace();
-
-	/* var_dump($s); */
-
-	if (!isset($s->counter))
-		$s->counter = 1000;
-	else
-		$s->counter++;
-	
-	$this->view->counter = $s->counter;
-
-	if ($s->counter > 1999)
-		unset($s->counter);
     }
 
     public function indexAction()
     {
         
-    }
-
-    public function loggedInUser()
-    {
-	$auth = Zend_Auth::getInstance();
-
-	if ($auth->hasIdentity()) {
-		$user = $auth->getIdentity();
-
-		$string = "Logged in as $user";
-
-	} else {
-		$string = "Not logged in";
-	}
-
-	return $string;
     }
 }
